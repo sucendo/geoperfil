3ba49a4f810139b51f06169a9f2e7fae5bd3c134
sucendo <samuelug.pro@gmail.com>
2023-05-24 15:28:57 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,29 @@
 // Función para cambiar de página
 function navigateTo(page) {
   const root = document.getElementById('root');
+  root.innerHTML = '';
+
   if (page === 'home') {
     root.innerHTML = '<h1>Hola desde JavaScript</h1>';
   } else if (page === 'about') {
     root.innerHTML = '<h2>Acerca de</h2><p>Esta es la página de Acerca de...</p>';
-  } else if (page === 'about') {
+  } else if (page === 'contact') {
     root.innerHTML = '<h2>Contacto</h2><p>Esta es la página de contacto.</p>';
+  } else if (page === 'quiz') {
+    loadQuiz(root);
   }
+}
+
+// Función para cargar el contenido del quiz
+function loadQuiz(container) {
+  const quizHTML = `
+    <section>
+      <h2>Quiz</h2>
+      <!-- Aquí va el contenido HTML del quiz -->
+    </section>
+  `;
+  container.innerHTML = quizHTML;
+  // Puedes agregar el código del quiz aquí
 }
 
 // Manejo de eventos de navegación
