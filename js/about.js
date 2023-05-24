58495ee7a1f9f280d609baed144a2d107336b948
sucendo <samuelug.pro@gmail.com>
2023-05-24 12:24:19 +0200

    Create about.js

---
+++ js/about.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h2>Acerca de</h2>
+      <p>Esta es la página de Acerca de.</p>
+    </div>
+  );
+};
+
+export default About;
