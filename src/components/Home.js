ee9c6e83ee0e74cc536246b96d2e458f41d61e21
sucendo <samuelug.pro@gmail.com>
2023-05-24 13:18:50 +0200

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
   return (
     <div>
-      <h1>Hola desde React en GitHub Pages</h1>
+      <h1>Hola desde React con Reach Router</h1>
     </div>
   );
 };
