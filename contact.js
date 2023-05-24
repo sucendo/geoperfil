061ba18155de3d4191dc3fc03bd56ec32a2e2f0d
sucendo <samuelug.pro@gmail.com>
2023-05-24 14:01:22 +0200

    Create contact.js

---
+++ contact.js
@@ -0,0 +1,5 @@
+export function loadContact(container) {
+  const contactDiv = document.createElement('div');
+  contactDiv.innerHTML = '<h2>Contacto</h2><p>Ponte en contacto con nosotros.</p>';
+  container.appendChild(contactDiv);
+}
