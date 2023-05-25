c3943db5643167142a8e31862e28a36f585292ac
sucendo <samuelug.pro@gmail.com>
2023-05-25 11:05:03 +0200

    Creamos concordancia entre preguntas y respuestas

--- quiz.js
+++ quiz.js
@@ -247,6 +247,69 @@ const allQuestions = [
   }
 ];
 
+
+// Función para obtener una selección aleatoria de preguntas
+function getRandomQuestions(totalQuestions) {
+  const shuffledQuestions = allQuestions.sort(() => 0.5 - Math.random());
+  return shuffledQuestions.slice(0, totalQuestions);
+}
+
+// Función para cargar el contenido del quiz en el contenedor
+function loadQuiz(container) {
+  container.innerHTML = '';
+
+  const totalQuestions = 10;
+  const questions = getRandomQuestions(totalQuestions);
+
+  // Generar las preguntas y opciones del quiz
+  questions.forEach(function(question, index) {
+    const questionElement = document.createElement('div');
+    questionElement.classList.add('question');
+    questionElement.innerHTML = `<h3>${index + 1}. ${question.question}</h3>`;
+
+    question.options.forEach(function(option, optionIndex) {
+      const optionElement = document.createElement('div');
+      optionElement.classList.add('option');
+      optionElement.innerHTML = `
+        <input type="radio" id="option-${index}-${optionIndex}" name="question-${index}" value="${optionIndex}">
+        <label for="option-${index}-${optionIndex}">${option}</label>
+      `;
+      questionElement.appendChild(optionElement);
+    });
+
+    container.appendChild(questionElement);
+  });
+
+  // Agregar botón de envío de respuestas
+  const submitButton = document.createElement('button');
+  submitButton.innerText = 'Enviar Respuestas';
+  submitButton.addEventListener('click', checkAnswers);
+  container.appendChild(submitButton);
+}
+
+// Función para verificar las respuestas seleccionadas por el usuario
+function checkAnswers() {
+  const answerElements = document.querySelectorAll('input[type="radio"]:checked');
+  const userAnswers = Array.from(answerElements).map(function(element) {
+    return parseInt(element.value);
+  });
+
+  let correctCount = 0;
+  userAnswers.forEach(function(userAnswer, index) {
+    const question = questions[index];
+    if (userAnswer === question.correctAnswer) {
+      correctCount++;
+    }
+  });
+
+  const resultMessage = `Obtuviste ${correctCount} respuestas correctas de ${questions.length}.`;
+  alert(resultMessage);
+}
+
+// Exportar la función loadQuiz para poder utilizarla en main.js
+export { loadQuiz };
+
+/*
 // Elementos del DOM
 const root = document.getElementById('root');
 const quizContainer = document.createElement('div');
@@ -377,3 +440,4 @@ function showResults() {
 
 // Iniciar el juego al cargar la página
 startGame();
+*/
