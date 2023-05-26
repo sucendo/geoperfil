1a5bc204566489fa85fa7c3b30516c1b582bd425
sucendo <samuelug.pro@gmail.com>
2023-05-26 12:30:17 +0200

    Create questions-kids.js

---
+++ questions-kids.js
@@ -0,0 +1,77 @@
+const questions = [
+  {
+    question: "¿Cuántas patas tiene una araña?",
+    options: ["4 patas", "6 patas", "8 patas", "10 patas"],
+    correctAnswer: 2
+  },
+  {
+    question: "¿Cuántos dedos tiene una mano?",
+    options: ["3 dedos", "4 dedos", "5 dedos", "6 dedos"],
+    correctAnswer: 2
+  },
+  {
+    question: "¿Cuántos días tiene una semana?",
+    options: ["3 días", "5 días", "7 días", "9 días"],
+    correctAnswer: 2
+  },
+  {
+    question: "¿Cuál es el color primario que se obtiene mezclando azul y amarillo?",
+    options: ["Rojo", "Verde", "Naranja", "Morado"],
+    correctAnswer: 2
+  },
+  {
+    question: "¿Cuántas estaciones del año hay?",
+    options: ["2 estaciones", "3 estaciones", "4 estaciones", "5 estaciones"],
+    correctAnswer: 2
+  },
+  {
+    question: "¿Cuántas patas tiene un perro?",
+    options: ["2 patas", "4 patas", "6 patas", "8 patas"],
+    correctAnswer: 1
+  },
+  {
+    question: "¿Cuántos planetas hay en nuestro sistema solar?",
+    options: ["5 planetas", "7 planetas", "8 planetas", "9 planetas"],
+    correctAnswer: 2
+  },
+  {
+    question: "¿Cuántos lados tiene un triángulo?",
+    options: ["2 lados", "3 lados", "4 lados", "5 lados"],
+    correctAnswer: 1
+  },
+  {
+    question: "¿Cuál es el resultado de sumar 2 + 3?",
+    options: ["3", "4", "5", "6"],
+    correctAnswer: 2
+  },
+  {
+    question: "¿Cuántos dedos tiene un pie?",
+    options: ["3 dedos", "4 dedos", "5 dedos", "6 dedos"],
+    correctAnswer: 2
+  },
+  {
+    question: "¿Cuál es la capital de España?",
+    options: ["Madrid", "Barcelona", "Valencia", "Sevilla"],
+    correctAnswer: 0
+  },
+  {
+    question: "¿Cuántas provincias hay en España?",
+    options: ["37 provincias", "48 provincias", "50 provincias", "52 provincias"],
+    correctAnswer: 0
+  },
+  {
+    question: "¿Cuántos días tiene el mes de marzo?",
+    options: ["28 días", "29 días", "30 días", "31 días"],
+    correctAnswer: 3
+  },
+  {
+    question: "¿Cuántas sílabas tiene la palabra 'elefante'?",
+    options: ["2 sílabas", "3 sílabas", "4 sílabas", "5 sílabas"],
+    correctAnswer: 2
+  },
+  {
+    question: "¿Cuál es el planeta más grande del sistema solar?",
+    options: ["Júpiter", "Saturno", "Urano", "Neptuno"],
+    correctAnswer: 0
+  }
+];
