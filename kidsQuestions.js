4a6e74b25e74cbf24c3d4f76ab847f2fd1fdc759
sucendo <samuelug.pro@gmail.com>
2023-05-26 14:26:57 +0200

    Update kidsQuestions.js

--- kidsQuestions.js
+++ kidsQuestions.js
@@ -73,5 +73,80 @@ const kidsQuestions = [
     question: "¿Cuál es el planeta más grande del sistema solar?",
     options: ["Júpiter", "Saturno", "Urano", "Neptuno"],
     correctAnswer: 0
+  },
+  {
+    question: "¿Cuántos huesos tiene el cuerpo humano?",
+    options: ["100 huesos", "206 huesos", "300 huesos", "500 huesos"],
+    correctAnswer: 1
+  },
+  {
+    question: "¿Cuál es el animal más grande del mundo?",
+    options: ["Elefante", "Ballena azul", "Jirafa", "Hipopótamo"],
+    correctAnswer: 1
+  },
+  {
+    question: "¿Cuántos dientes temporales tiene un niño?",
+    options: ["10 dientes", "16 dientes", "20 dientes", "24 dientes"],
+    correctAnswer: 2
+  },
+  {
+    question: "¿Cuál es la capital de Francia?",
+    options: ["París", "Londres", "Berlín", "Roma"],
+    correctAnswer: 0
+  },
+  {
+    question: "¿Cuántos continentes hay en el mundo?",
+    options: ["4 continentes", "5 continentes", "6 continentes", "7 continentes"],
+    correctAnswer: 3
+  },
+  {
+    question: "¿Cuántos jugadores hay en un equipo de fútbol?",
+    options: ["9 jugadores", "11 jugadores", "13 jugadores", "15 jugadores"],
+    correctAnswer: 1
+  },
+  {
+    question: "¿Cuál es el animal que vuela más alto?",
+    options: ["Aguila", "Murciélago", "Colibrí", "Águila real"],
+    correctAnswer: 0
+  },
+  {
+    question: "¿Cuántas patas tiene una mariposa?",
+    options: ["2 patas", "4 patas", "6 patas", "8 patas"],
+    correctAnswer: 2
+  },
+  {
+    question: "¿Cuántos segundos hay en un minuto?",
+    options: ["30 segundos", "45 segundos", "60 segundos", "75 segundos"],
+    correctAnswer: 2
+  },
+  {
+    question: "¿Cuál es el océano más grande del mundo?",
+    options: ["Océano Atlántico", "Océano Índico", "Océano Pacífico", "Océano Ártico"],
+    correctAnswer: 2
+  },
+  {
+    question: "¿Cuántos lados tiene un cuadrado?",
+    options: ["2 lados", "3 lados", "4 lados", "5 lados"],
+    correctAnswer: 2
+  },
+  {
+    question: "¿Cuántos colores tiene el arcoíris?",
+    options: ["3 colores", "5 colores", "7 colores", "9 colores"],
+    correctAnswer: 2
+  },
+  {
+    question: "¿Cuántos años tiene un lustro?",
+    options: ["3 años", "5 años", "7 años", "10 años"],
+    correctAnswer: 1
+  },
+  {
+    question: "¿Cuántas provincias hay en Andalucía?",
+    options: ["4 provincias", "6 provincias", "8 provincias", "10 provincias"],
+    correctAnswer: 2
+  },
+  {
+    question: "¿Cuál es la capital de Italia?",
+    options: ["París", "Londres", "Roma", "Berlín"],
+    correctAnswer: 2
   }
 ];
