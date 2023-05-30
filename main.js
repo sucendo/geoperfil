778b98ede4f0ac03e4a175ad17320a3663f8c1e4
sucendo <samuelug.pro@gmail.com>
2023-05-30 09:26:18 +0200

    Update main.js

--- main.js
+++ main.js
@@ -209,45 +209,62 @@ function loadKids() {
       }
 
       // Función para mostrar los resultados
-      function showResults() {
-        quizContainer.innerHTML = '<h2>Resultados</h2>';
+      // Función para mostrar los resultados
+function showResults() {
+  quizContainer.innerHTML = '<h2>Resultados</h2>';
 
-        let correctCount = 0;
+  let correctCount = 0;
 
-        quizQuestions.forEach(function(question, index) {
-          const questionElement = document.createElement('div');
-          questionElement.classList.add('result');
+  quizQuestions.forEach(function(question, index) {
+    const questionElement = document.createElement('div');
+    questionElement.classList.add('result');
+    questionElement.innerHTML = `<h3>${index + 1}. ${question.question}</h3>`;
 
-          questionElement.innerHTML = `<h3>${index + 1}. ${question.question}</h3>`;
+    const userAnswer = question.hasOwnProperty('userAnswer') ? question.userAnswer : null;
+    const correctAnswer = question.correctAnswer;
+    const isCorrect = userAnswer === correctAnswer;
 
-          const userAnswer = question.userAnswer;
-          const correctAnswer = question.correctAnswer;
+    if (isCorrect) {
+      correctCount++;
+      questionElement.classList.add('correct');
+    } else {
+      questionElement.classList.add('incorrect');
+      const correctOptionIndex = question.options.findIndex((option, index) => index === correctAnswer);
+      const correctOptionElement = questionElement.querySelector(`#result-option-${correctOptionIndex}`);
+      correctOptionElement.classList.add('correct-answer');
+    }
 
-          if (userAnswer === correctAnswer) {
-            questionElement.classList.add('correct');
-            correctCount++;
-          } else {
-            questionElement.classList.add('incorrect');
-          }
+    const options = question.options.map(function(option, optionIndex) {
+      const optionElement = document.createElement('div');
+      optionElement.classList.add('option');
+      optionElement.innerHTML = `
+        <input type="radio" id="result-option-${optionIndex}" name="result-question-${index}" disabled>
+        <label for="result-option-${optionIndex}">${option}</label>
+      `;
 
-          const options = question.options.map((option, optionIndex) => {
-            const optionElement = document.createElement('div');
-            optionElement.innerHTML = `
-              <input type="radio" disabled ${optionIndex === userAnswer ? 'checked' : ''}>
-              <label>${option}</label>
-            `;
-            return optionElement.outerHTML;
-          });
-
-          questionElement.innerHTML += options.join('');
-
-          quizContainer.appendChild(questionElement);
-        });
-
-        const resultMessage = document.createElement('p');
-        resultMessage.innerText = `Obtuviste ${correctCount} respuestas correctas de ${quizQuestions.length}.`;
-        quizContainer.appendChild(resultMessage);
+      if (optionIndex === userAnswer) {
+        optionElement.classList.add('selected');
       }
+
+      if (optionIndex === correctAnswer) {
+        optionElement.classList.add('correct-answer');
+      }
+
+      return optionElement;
+    });
+
+    options.forEach(function(option) {
+      questionElement.appendChild(option);
+    });
+
+    quizContainer.appendChild(questionElement);
+  });
+
+  const scoreElement = document.createElement('p');
+  scoreElement.classList.add('score');
+  scoreElement.innerText = `Obtuviste ${correctCount} respuestas correctas de ${currentQuestionIndex}.`;
+  quizContainer.appendChild(scoreElement);
+}
 
       // Obtener las preguntas para el juego
       quizQuestions = getRandomQuestions(10); // Obtener 10 preguntas aleatorias
@@ -256,38 +273,6 @@ function loadKids() {
       showQuestion();
     })
     .catch(error => console.log('Error al cargar el archivo kidsQuestions.js:', error));
-}
-
-// Función para verificar las respuestas del quiz
-// Función para verificar las respuestas seleccionadas por el usuario
-function checkAnswers() {
-  const root = document.getElementById('root');
-  const quizContainer = root.querySelector('.quiz-container');
-
-  const selectedOptions = quizContainer.querySelectorAll('input[type="radio"]:checked');
-  const userAnswers = Array.from(selectedOptions).map(option => parseInt(option.value));
-
-  let correctCount = 0;
-
-  quizQuestions.forEach(function(question, index) {
-    const userAnswer = userAnswers[index];
-    const correctAnswer = question.correctAnswer;
-
-    const questionElement = quizContainer.querySelector(`[name="question-${index}"]`);
-
-    if (userAnswer === correctAnswer) {
-      questionElement.classList.add('correct');
-      correctCount++;
-    } else {
-      questionElement.classList.add('incorrect');
-    }
-
-    questionElement.disabled = true;
-  });
-
-  const resultMessage = document.createElement('p');
-  resultMessage.innerText = `Obtuviste ${correctCount} respuestas correctas de ${quizQuestions.length}.`;
-  quizContainer.appendChild(resultMessage);
 }
 
 // Manejo de eventos de navegación
