937f6a2904d2725c886277d30cd2a11a1d040334
sucendo <samuelug.pro@gmail.com>
2023-05-31 12:00:27 +0200

    Update main.js

--- main.js
+++ main.js
@@ -6,13 +6,13 @@ function navigateTo(page) {
   if (page === 'home') {
     root.innerHTML = '<h1>Hola amig@</h1><object data="hombre.svg" type="image/svg+xml"></object>';
   } else if (page === 'about') {
-    root.innerHTML = '<h2>Acerca de</h2><p>Mi nombre es Sucendo.</p><p>Soy desarrollador informático.</p><p>He desarrollado este juego para mis 5 hijos.</p>';
+    root.innerHTML = '<h2>Acerca de</h2><p>Mi nombre es Sucendo.</p><p>Soy desarrollador informático.</p><p>He desarrollado este juego para mis 5 hijos.</p>';  
   } else if (page === 'quiz') {
     loadQuiz("questions.js");
   } else if (page === 'kids') {
     loadQuiz("questionsKids.js");
   } else if (page === 'blog') {
-    root.innerHTML = '<h2>Blog</h2><p>Este es mi blog dinámico</p><p></p><p></p>';
+    root.innerHTML = '<h2>Blog</h2><p>Este es mi blog dinámico</p><p></p><p></p>';  
   } else if (page === 'contact') {
     root.innerHTML = '<h2>Contacto</h2><p>Esta es la página de contacto. Pero...</p>';
   }
@@ -26,22 +26,30 @@ function loadScript(scriptPath, callback) {
   document.head.appendChild(script);
 }
 
-// Función para cargar el juego de preguntas
 function loadQuiz(questions) {
   const root = document.getElementById('root');
   root.innerHTML = '';
 
-  // Crear contenedor para el juego
-  const quizContainer = document.createElement('div');
-  quizContainer.classList.add('quiz-container');
-  root.appendChild(quizContainer);
-
-  // Cargar el archivo questions.js de forma dinámica
+  // Cargar el archivo quiz.js de forma dinámica
   loadScript(questions, function() {
-    // Llamar a la función startGame del archivo quiz.js dentro de una función anónima
+    // Llamar a la función startGame del archivo quiz.js
     startGame(questions);
   });
 }
+
+// Función para cargar el contenido del quiz para niños en el contenedor
+/*
+function loadKids() {
+  const root = document.getElementById('root');
+  root.innerHTML = '';
+
+  // Cargar el archivo kidsQuiz.js de forma dinámica
+  loadScript('kidsQuiz.js', function() {
+    // Llamar a la función startGame del archivo kidsQuiz.js
+    startGame();
+  });
+}
+*/
 
 // Manejo de eventos de navegación
 document.addEventListener('DOMContentLoaded', function() {
