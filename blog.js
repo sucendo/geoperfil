fa539e4d49eaf889b53fbf317a8d6156f8122aa0
sucendo <samuelug.pro@gmail.com>
2023-05-31 13:53:47 +0200

    Update and rename blog.json to blog.js

---
+++ blog.js
@@ -0,0 +1,14 @@
+// blog.js
+const blogData = {
+  articles: [
+    {
+      title: 'Título del artículo 1',
+      content: 'Contenido del artículo 1'
+    },
+    {
+      title: 'Título del artículo 2',
+      content: 'Contenido del artículo 2'
+    },
+    // ... otros artículos
+  ]
+};
